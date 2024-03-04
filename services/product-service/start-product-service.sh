94c59efb177289768dad94207c9979553069f71a
Eric Kivel <ericvkivel1@gmail.com>
2024-03-04 01:37:56 -0300

    fix: new relic docker integration

--- services/product-service/start-product-service.sh
+++ services/product-service/start-product-service.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn -w 2 -b 0.0.0.0:3002 src.wsgi:app
+# NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn -w 2 -b 0.0.0.0:3002 src.wsgi:app
+NEW_RELIC_CONFIG_FILE=services/product-service/newrelic.ini newrelic-admin run-program gunicorn --chdir services/product-service -w 2 -b 0.0.0.0:3002 src.wsgi:app
